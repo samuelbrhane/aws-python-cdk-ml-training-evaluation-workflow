d29d911f82d240893e174ff96618d6c6cc2f50b9
samuelbrhanealemayohu <contact@samuelbrhane.com>
2026-02-08 13:40:26 +0300

    feat: add SageMaker training job to MlA2TrainingEvaluationStack

--- ml_a2/stack.py
+++ ml_a2/stack.py
@@ -105,3 +105,58 @@ class MlA2TrainingEvaluationStack(Stack):
             subject="ML-A2 Training + Evaluation Pipeline Failed",
             message=sfn.TaskInput.from_json_path_at("$"),
         )
+
+
+        # Step 1: SageMaker Training job
+        training_job_name = sfn.JsonPath.format(
+            "ml-a2-train-{}",
+            sfn.JsonPath.string_at("$$.Execution.Name"),
+        )
+
+        model_output_s3 = sfn.JsonPath.format(
+            f"s3://{self.model_bucket.bucket_name}/models/" + "{}",
+            sfn.JsonPath.string_at("$$.Execution.Name"),
+        )
+
+        train_task = tasks.SageMakerCreateTrainingJob(
+            self,
+            "TrainModel",
+            integration_pattern=sfn.IntegrationPattern.RUN_JOB,
+            training_job_name=training_job_name,
+            role=self.sagemaker_execution_role,
+            algorithm_specification=tasks.AlgorithmSpecification(
+                training_image=tasks.DockerImage.from_registry(
+                    "public.ecr.aws/sagemaker/sagemaker-xgboost:1.7-1"
+                ),
+                training_input_mode=tasks.InputMode.FILE,
+            ),
+            input_data_config=[
+                tasks.Channel(
+                    channel_name="train",
+                    data_source=tasks.DataSource(
+                        s3_data_source=tasks.S3DataSource(
+                            s3_location=tasks.S3Location.from_uri(
+                                f"s3://{self.training_data_bucket.bucket_name}/train/"
+                            ),
+                            s3_data_type=tasks.S3DataType.S3_PREFIX,
+                        )
+                    ),
+                    content_type="text/csv",
+                )
+            ],
+            output_data_config=tasks.OutputDataConfig(
+                s3_output_location=tasks.S3Location.from_uri(model_output_s3)
+            ),
+            resource_config=tasks.ResourceConfig(
+                instance_count=1,
+                instance_type=ec2.InstanceType("ml.m5.large"),
+                volume_size=Size.gibibytes(30),
+            ),
+            stopping_condition=tasks.StoppingCondition(max_runtime=Duration.hours(1)),
+            hyperparameters={
+                "max_depth": "5",
+                "eta": "0.2",
+                "objective": "binary:logistic",
+                "num_round": "20",
+            },
+        )
